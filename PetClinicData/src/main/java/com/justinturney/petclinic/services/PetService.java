9d2d812e5601a40a060f7d0ceaa0219a12359843
greenfructose <justinclarkturney@gmail.com>
2022-05-24 11:28:28 -0500

    Removed unused imports. closes #12

--- PetClinicData/src/main/java/com/justinturney/petclinic/services/PetService.java
+++ PetClinicData/src/main/java/com/justinturney/petclinic/services/PetService.java
@@ -2,7 +2,6 @@ package com.justinturney.petclinic.services;
 
 import com.justinturney.petclinic.model.Pet;
 
-import java.util.Set;
 
 public interface PetService extends CrudService<Pet, Long>{
 
--- PetClinicData/src/main/java/com/justinturney/petclinic/services/VetService.java
+++ PetClinicData/src/main/java/com/justinturney/petclinic/services/VetService.java
@@ -2,7 +2,6 @@ package com.justinturney.petclinic.services;
 
 import com.justinturney.petclinic.model.Vet;
 
-import java.util.Set;
 
 public interface VetService extends CrudService<Vet, Long> {
 
