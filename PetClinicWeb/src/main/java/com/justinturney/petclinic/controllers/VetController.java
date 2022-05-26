7b1078ebbf81b7e78045874f41d6cf81bab4da1b
greenfructose <justinclarkturney@gmail.com>
2022-05-26 11:41:28 -0500

    Added web binding to disallow manipulation of id field. closes #66

--- PetClinicWeb/src/main/java/com/justinturney/petclinic/controllers/VetController.java
+++ PetClinicWeb/src/main/java/com/justinturney/petclinic/controllers/VetController.java
@@ -3,6 +3,8 @@ package com.justinturney.petclinic.controllers;
 import com.justinturney.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/vets", "/vets.html"})
@@ -13,6 +15,11 @@ public class VetController {
 
     public VetController(VetService vetService) {
         this.vetService = vetService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({ "", "/", "/index", "/index.html"})
